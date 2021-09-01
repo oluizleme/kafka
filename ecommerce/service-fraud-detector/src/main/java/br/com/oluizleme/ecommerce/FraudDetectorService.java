48c0eea19d10ca590794c892499c24724f890d44
Luiz Henrique Leme <67121985+souluizhenriqueleme@users.noreply.github.com>
2021-09-01 00:40:38 -0300

    Implementado o modulo de deteccao de fraude de uma ordem

--- ecommerce/service-fraud-detector/src/main/java/br/com/oluizleme/ecommerce/FraudDetectorService.java
+++ ecommerce/service-fraud-detector/src/main/java/br/com/oluizleme/ecommerce/FraudDetectorService.java
@@ -1,2 +1,54 @@
-package br.com.oluizleme.ecommerce;public class FraudDetectorService {
+package br.com.oluizleme.ecommerce;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+
+import java.math.BigDecimal;
+import java.util.Map;
+import java.util.concurrent.ExecutionException;
+
+public class FraudDetectorService {
+
+    public static void main(String[] args) {
+        var fraudDetectorService = new FraudDetectorService();
+        try(var service = new KafkaService(FraudDetectorService.class.getSimpleName(),
+                "ECOMMERCE_NEW_ORDER",
+                fraudDetectorService::parse,
+                Order.class,
+                Map.of())){
+            service.run();
+        }
+    }
+
+    private final KafkaDispatcher<Order> orderDispatcher = new KafkaDispatcher<Order>();
+
+    private void parse(ConsumerRecord<String,Order> record) throws ExecutionException, InterruptedException {
+        System.out.println("--------------------------------------------------------");
+        System.out.println("Processing new order, checking for fraud.");
+        System.out.println("KEY " + record.key());
+        System.out.println("VALUE " + record.value());
+        System.out.println("PARTITION " + record.partition());
+        System.out.println("OFFSET " + record.offset());
+
+        try{
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        var order = record.value();
+
+        if(isFraud(order)) {
+            //pretending thar the fraud happens when the amount is > = 4500
+            System.out.println("Order is a fraud!!! " + order.toString());
+            orderDispatcher.send("ECOMMERCE_ORDER_REJECTED", order.getEmail(), order);
+        } else {
+            System.out.println("Order approved: " + order.toString());
+            orderDispatcher.send("ECOMMERCE_ORDER_APPROVED", order.getEmail(), order);
+        }
+        System.out.println("Order processed");
+    }
+
+    private boolean isFraud(Order order) {
+        return order.getAmount().compareTo(new BigDecimal(4500)) >= 0 ;
+    }
 }
--- ecommerce/service-fraud-detector/src/main/java/br/com/oluizleme/ecommerce/Order.java
+++ ecommerce/service-fraud-detector/src/main/java/br/com/oluizleme/ecommerce/Order.java
@@ -13,4 +13,21 @@ public class Order {
         this.amount = amount;
         this.email = email;
     }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    @Override
+    public String toString() {
+        return "Order{" +
+                "orderId='" + orderId + '\'' +
+                ", amount=" + amount +
+                ", email='" + email + '\'' +
+                '}';
+    }
 }
