74fcb0caec248a76f0e47786f25cbcfbd74c4baf
Luiz Henrique Leme <67121985+souluizhenriqueleme@users.noreply.github.com>
2021-09-01 01:01:40 -0300

    Criado o EmailService cosumer do topico ECOMMERCE_NEW_ORDER

--- ecommerce/service-email/src/main/java/br/com/oluizleme/ecommerce/EmailService.java
+++ ecommerce/service-email/src/main/java/br/com/oluizleme/ecommerce/EmailService.java
@@ -1,2 +1,33 @@
-package br.com.oluizleme.ecommerce;public class EmailService {
+package br.com.oluizleme.ecommerce;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+
+import java.util.Map;
+
+public class EmailService {
+
+    public static void main(String[] args) {
+        var emailService = new EmailService();
+        try (var service = new KafkaService<String>(EmailService.class.getSimpleName(),
+                "ECOMMERCE_SEND_EMAIL",
+                emailService::parse,
+                String.class,
+                Map.of())) {
+            service.run();
+        }
+    }
+
+    private void parse(ConsumerRecord<String, String> record) {
+        System.out.println("--------------------------------------------------------");
+        System.out.println("Send email");
+        System.out.println("KEY " + record.key());
+        System.out.println("VALUE " + record.value());
+        System.out.println("PARTITION " + record.partition());
+        System.out.println("OFFSET " + record.offset());
+        try{
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
 }
