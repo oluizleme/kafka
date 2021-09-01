e0d2709d910ccd8cbcd5f0dc9d6afdc317bfd5ca
Luiz Henrique Leme <85969725+oluizleme@users.noreply.github.com>
2021-09-01 15:40:09 -0300

    Implementacao do modulo de processamento de relatorios

--- ecommerce/service-reading-report/src/main/java/br/com/oluizleme/ecommerce/User.java
+++ ecommerce/service-reading-report/src/main/java/br/com/oluizleme/ecommerce/User.java
@@ -1,2 +1,18 @@
-package br.com.oluizleme.ecommerce;public class User {
+package br.com.oluizleme.ecommerce;
+
+public class User {
+
+    private final String uuid;
+
+    public User(String uuid) {
+        this.uuid = uuid;
+    }
+
+    public String getUuid() {
+        return uuid;
+    }
+
+    public String getReportPath() {
+        return "target/" + uuid + "-report.txt";
+    }
 }
