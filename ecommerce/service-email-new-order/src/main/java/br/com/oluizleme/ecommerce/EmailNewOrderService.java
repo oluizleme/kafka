64bf72f44f51ada608dec98719ae88a7a2d220f2
Luiz Henrique Leme <85969725+oluizleme@users.noreply.github.com>
2021-09-02 21:51:57 -0300

    Ajustado a forma de consumo

--- ecommerce/service-email-new-order/src/main/java/br/com/oluizleme/ecommerce/EmailNewOrderService.java
+++ ecommerce/service-email-new-order/src/main/java/br/com/oluizleme/ecommerce/EmailNewOrderService.java
@@ -1,28 +1,21 @@
 package br.com.oluizleme.ecommerce;
 
-import br.com.oluizleme.ecommerce.consumer.KafkaService;
+import br.com.oluizleme.ecommerce.consumer.ConsumerService;
+import br.com.oluizleme.ecommerce.consumer.ServiceRunner;
 import br.com.oluizleme.ecommerce.dispatcher.KafkaDispatcher;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 
-import java.io.IOException;
-import java.util.Map;
 import java.util.concurrent.ExecutionException;
 
-public class EmailNewOrderService {
+public class EmailNewOrderService implements ConsumerService<Order> {
 
-    public static void main(String[] args) throws ExecutionException, InterruptedException, IOException {
-        var emailNewOrderService = new EmailNewOrderService();
-        try(var service = new KafkaService(EmailNewOrderService.class.getSimpleName(),
-                "ECOMMERCE_NEW_ORDER",
-                emailNewOrderService::parse,
-                Map.of())){
-            service.run();
-        }
+    public static void main(String[] args) {
+        new ServiceRunner(EmailNewOrderService::new).start(1);
     }
 
     private final KafkaDispatcher<String> emailDispatcher = new KafkaDispatcher<String>();
 
-    private void parse(ConsumerRecord<String, Message<Order>> record) throws ExecutionException, InterruptedException {
+    public void parse(ConsumerRecord<String, Message<Order>> record) throws ExecutionException, InterruptedException {
         System.out.println("--------------------------------------------------------");
         System.out.println("Processing new order, preparing email");
         var message = record.value();
@@ -33,5 +26,15 @@ public class EmailNewOrderService {
         var id = message.getId().continueWith(EmailNewOrderService.class.getSimpleName());
         emailDispatcher.send("ECOMMERCE_SEND_EMAIL", order.getEmail(),
                 id, emailCode);
+    }
+
+    @Override
+    public String getTopic() {
+        return "ECOMMERCE_NEW_ORDER";
+    }
+
+    @Override
+    public String getConsumerGroup() {
+        return EmailNewOrderService.class.getSimpleName();
     }
 }
